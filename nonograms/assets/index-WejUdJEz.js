e06af8be6e3e88e8a98e9abe67e0bbafbe6e443a
zagorky <zagorkiynahyi@gmail.com>
2025-01-26 13:41:39 +0300

    Updates

--- nonograms/assets/index-WejUdJEz.js
+++ nonograms/assets/index-WejUdJEz.js
@@ -430,10 +430,10 @@ class Cell extends BaseComponent {
   }
 }
 
-const gameBoard = "_gameBoard_drtco_1";
-const easy = "_easy_drtco_9";
-const medium = "_medium_drtco_14";
-const hard = "_hard_drtco_19";
+const gameBoard = "_gameBoard_127lz_1";
+const easy = "_easy_127lz_9";
+const medium = "_medium_127lz_14";
+const hard = "_hard_127lz_19";
 const styles$2 = {
 	gameBoard: gameBoard,
 	easy: easy,
@@ -1523,4 +1523,4 @@ class Wrapper extends BaseComponent {
 
 const root = new Wrapper();
 root.init();
-//# sourceMappingURL=index-D_sr68kS.js.map
+//# sourceMappingURL=index-WejUdJEz.js.map
