316ac04d7021ef78aa89f1a20c66c1b7fecf440a
zagorky <zagorkiynahyi@gmail.com>
2025-03-01 14:53:30 +0300

    feat: add decision picker page

--- decision-making-tool/src/pages/decision-picker.ts
+++ decision-making-tool/src/pages/decision-picker.ts
@@ -1,1 +1,20 @@
-console.log('');
+import { H1, Input, Label, Section } from '../utils/factory.ts';
+import { createRotationButton, wheel } from '../components/Wheel/wheel.ts';
+
+function decisionPickerPage(): HTMLElement {
+  return Section([drawHeading(), drawTimerInput(), wheel.canvas, createRotationButton()]);
+}
+
+function drawHeading(): HTMLHeadingElement {
+  return H1('Decision Making Tool');
+}
+
+function drawTimerInput(): HTMLLabelElement {
+  return Label(['Set duration', drawInput()], 'timer');
+}
+
+function drawInput(): HTMLInputElement {
+  return Input('Timer', 'timer');
+}
+
+export { decisionPickerPage };
