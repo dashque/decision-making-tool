1439af1cdac20cbe021fc87fb0f6685aeaa95456
zagorky <zagorkiynahyi@gmail.com>
2025-03-01 14:36:12 +0300

    feat: start do index page

---
+++ decision-making-tool/src/pages/index.ts
@@ -0,0 +1,55 @@
+import { Button, H1, Section } from '../utils/factory.ts';
+
+function mainPage(): HTMLElement {
+  return Section([
+    drawHeading(),
+    drawAddOptionButton(),
+    drawPasteListButton(),
+    drawClearListButton(),
+    drawSaveListButton(),
+    drawLoadFromListButton(),
+    drawStartButton(),
+  ]);
+}
+
+function drawHeading(): HTMLHeadingElement {
+  return H1('Decision Making Tool');
+}
+
+function drawAddOptionButton(): HTMLButtonElement {
+  const button = Button('Add Options');
+  //add event listener
+  return button;
+}
+
+function drawPasteListButton(): HTMLButtonElement {
+  const button = Button('Paste List');
+  //add event listener
+  return button;
+}
+
+function drawClearListButton(): HTMLButtonElement {
+  const button = Button('Clear List');
+  //add event listener
+  return button;
+}
+
+function drawSaveListButton(): HTMLButtonElement {
+  const button = Button('Save List to File');
+  //add event listener
+  return button;
+}
+
+function drawLoadFromListButton(): HTMLButtonElement {
+  const button = Button('Load List from File');
+  //add event listener
+  return button;
+}
+
+function drawStartButton(): HTMLButtonElement {
+  const button = Button('Start');
+  //add event listener
+  return button;
+}
+
+export { mainPage };
