38dabe65225e2d0e52b692f572674f61e0ca076f
zagorky <zagorkiynahyi@gmail.com>
2025-03-02 10:53:22 +0300

    style: add styles to elems

--- decision-making-tool/src/utils/factory.ts
+++ decision-making-tool/src/utils/factory.ts
@@ -4,10 +4,17 @@ import { createElement } from './create-element.ts';
 const H1 = (children: Children): HTMLHeadingElement =>
   createElement({
     tag: 'h1',
-    cssClasses: ['font-bold', 'text-pink-500', 'p-4', 'text-center'],
+    cssClasses: [
+      'text-2xl',
+      'font-bold',
+      'text-pink-600',
+      'p-3',
+      'text-center',
+    ],
     children,
   });
-const H2 = (children: Children): HTMLHeadingElement => createElement({ tag: 'h2', children });
+const H2 = (children: Children): HTMLHeadingElement =>
+  createElement({ tag: 'h2', children });
 const Main = (children: Children): HTMLElement =>
   createElement({
     tag: 'main',
@@ -17,21 +24,23 @@ const Main = (children: Children): HTMLElement =>
 const Section = (children: Children): HTMLElement =>
   createElement({
     tag: 'section',
-    cssClasses: ['flex', 'flex-col', 'justify-center', 'items-center', 'h-screen'],
+    cssClasses: ['flex', 'flex-col', 'justify-center', 'items-center'],
     children,
   });
-const Div = (children: Children): HTMLDivElement => createElement({ tag: 'div', children });
+const Div = (children: Children): HTMLDivElement =>
+  createElement({ tag: 'div', children });
 const Button = (children: Children): HTMLButtonElement => {
   return createElement({
     tag: 'button',
     cssClasses: [
+      'w-96',
       'px-4',
       'py-2',
-      'bg-blue-500',
+      'bg-emerald-400',
       'text-white',
       'rounded-lg',
-      'hover:bg-blue-700',
-      'm-4',
+      'hover:bg-emerald-700',
+      'm-2',
       'cursor-pointer',
     ],
     children,
@@ -45,22 +54,35 @@ const Input = (children: Children, id: string): HTMLInputElement =>
       'w-full',
       'px-4',
       'py-2',
+      'm-1',
       'border',
       'border-gray-300',
       'rounded-md',
       'shadow-sm',
       'focus:outline-none',
       'focus:ring-2',
-      'focus:ring-blue-500',
-      'focus:border-blue-500',
+      'focus:ring-pink-500',
+      'focus:border-pink-500',
       'transition',
       'duration-200',
     ],
     attributes: { id: `${id}` },
   });
 const Label = (children: Children, forLabel: string): HTMLLabelElement =>
-  createElement({ tag: 'label', children, attributes: { type: 'text', for: `${forLabel}` } });
-const Link = (children: Children, locationFrom: string, locationTo: string): HTMLAnchorElement =>
-  createElement({ tag: 'a', attributes: { href: `${locationFrom}${locationTo}` }, children });
+  createElement({
+    tag: 'label',
+    children,
+    attributes: { type: 'text', for: `${forLabel}` },
+  });
+const Link = (
+  children: Children,
+  locationFrom: string,
+  locationTo: string,
+): HTMLAnchorElement =>
+  createElement({
+    tag: 'a',
+    attributes: { href: `${locationFrom}${locationTo}` },
+    children,
+  });
 
 export { H1, H2, Main, Section, Div, Button, Input, Label, Link };
