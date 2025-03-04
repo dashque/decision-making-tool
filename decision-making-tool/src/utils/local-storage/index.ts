a95e5c6bb1f561d87b102fcc7d6c8710f3e2ba5d
zagorky <zagorkiynahyi@gmail.com>
2025-03-04 20:58:21 +0300

    feat: add local storage functions

---
+++ decision-making-tool/src/utils/local-storage/index.ts
@@ -0,0 +1,56 @@
+export type OptionList = ({ list: List } | { lastID: number })[];
+
+type List = {
+  id: string;
+  title: string;
+  weight: string;
+};
+
+export type SoundOption = Record<string, boolean>;
+
+export type DataLS = Record<string, OptionList | SoundOption>;
+
+export function isSoundOption(object: unknown): object is SoundOption {
+  if (typeof object !== 'object' || object === null) return false;
+  return Object.values(object).every((value) => typeof value === 'boolean');
+}
+
+export function isOptionList(object: unknown): object is OptionList {
+  if (!Array.isArray(object)) {
+    return false;
+  }
+
+  return object.every((item) => {
+    if (!isObject(item)) {
+      return false;
+    }
+
+    if ('list' in item) {
+      return isList(item.list);
+    }
+
+    if ('lastID' in item) {
+      return typeof item.lastID === 'number';
+    }
+
+    return false;
+  });
+}
+
+export function isList(object: unknown): object is List {
+  if (!isObject(object)) {
+    return false;
+  }
+  return (
+    'id' in object &&
+    'title' in object &&
+    'weight' in object &&
+    typeof object.id === 'string' &&
+    typeof object.title === 'string' &&
+    typeof object.weight === 'string'
+  );
+}
+
+function isObject(value: unknown): value is Record<string, unknown> {
+  return typeof value === 'object' && value !== null;
+}
---
+++ decision-making-tool/src/utils/local-storage/use-local-storage.ts
@@ -0,0 +1,38 @@
+import type { DataLS } from '~/utils/local-storage/index.ts';
+import { isOptionList, isSoundOption } from '~/utils/local-storage/index.ts';
+import { assertIsNonNullable } from '~/utils';
+
+// const data = {
+//   optionList: [{ list: { id: '#1', title: 'dasha', weight: '1' } }, { lastID: 1 }],
+//   sound: { on: true },
+// };
+
+function setDataToLS(data: DataLS): void {
+  Object.keys(data).forEach((key: string) => {
+    const storageKey = `zagorky:${key}`;
+    localStorage.setItem(storageKey, JSON.stringify(data[key]));
+  });
+}
+
+function getDataFromLS(): DataLS {
+  const result: DataLS = {};
+  const dataKeys = Object.keys(localStorage).filter((key) => key.startsWith('zagorky:'));
+
+  for (const key of dataKeys) {
+    const data = localStorage.getItem(key);
+    assertIsNonNullable(data);
+    const parsedData: unknown = JSON.parse(data);
+    const formattedKey = key.replace('zagorky:', '').trim();
+
+    if (isSoundOption(parsedData)) {
+      result[formattedKey] = parsedData;
+    } else if (isOptionList(parsedData)) {
+      result[formattedKey] = parsedData;
+    } else {
+      throw new Error('Error getting data from storage');
+    }
+  }
+  return result;
+}
+
+export { setDataToLS, getDataFromLS };
