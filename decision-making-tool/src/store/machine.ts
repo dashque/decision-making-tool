60da024bdf8a0a4da3ec79f31cc9869cf59ca819
zagorky <zagorkiynahyi@gmail.com>
2025-03-05 08:53:19 +0300

    feat: start think about state machine

---
+++ decision-making-tool/src/store/machine.ts
@@ -0,0 +1,80 @@
+import { getDataFromLS } from '~/utils/local-storage/local-storage-manager.ts';
+import { historyResolver } from '~/router.ts';
+
+const context = {
+  optionList: [{ list: { id: '#1', title: 'dasha', weight: '1' } }, { lastID: 1 }],
+  sound: { on: true },
+};
+
+const stateMachineDefinition = {
+  context: context,
+  initialState: 'initialState',
+  states: {
+    initialState: {
+      actions(): void {
+        const url = globalThis.location.hash || '#/';
+        historyResolver('initial', url);
+      },
+      transitions: {},
+    },
+    addOptionsState: {
+      actions(): void {
+        console.log('');
+      },
+      transitions: {},
+    },
+    pasteOptionsState: {
+      actions(): void {
+        console.log('');
+      },
+      transitions: {},
+    },
+    clearOptionsState: {
+      actions(): void {
+        console.log('');
+      },
+      transitions: {},
+    },
+    saveOptionsToLSState: {
+      actions: {
+        onEnter(): void {
+          console.log('');
+
+          // setDataToLS(this.context.optionList);
+        },
+      },
+      transitions: {},
+    },
+    loadOptionsFromLSState: {
+      actions(): void {
+        getDataFromLS();
+      },
+      transitions: {},
+    },
+    comeToDecisionPickerState: {
+      actions(): void {
+        //TODO перенести роутинг
+        // historyResolver('Main', link.getAttribute('href') ?? '#/')
+      },
+      transitions: {},
+    },
+    returnToMainState: {
+      actions(): void {
+        //TODO перенести роутинг
+        //historyResolver('Decision picker', link.getAttribute('href') ?? '#/decision-picker'),
+      },
+      transitions: {},
+    },
+    toggleSoundsState: {
+      actions(): void {
+        console.log('');
+      },
+      transitions: {},
+    },
+  },
+};
+
+console.log(stateMachineDefinition);
+
+// const machine = new StateMachine(stateMachineDefinition, context);
+// export { machine };
