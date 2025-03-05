08e8e515ff4a4d7bf7ec658d4397208299fcc310
zagorky <zagorkiynahyi@gmail.com>
2025-03-05 21:24:08 +0300

    refactor: rewrite sm definition

--- decision-making-tool/src/store/machine.ts
+++ decision-making-tool/src/store/machine.ts
@@ -1,4 +1,5 @@
 import { historyResolver } from '~/router.ts';
+import { getDataFromLS, setDataToLS } from '~/store/local-storage/local-storage-manager.ts';
 
 export const context = {
   optionList: [{ list: { id: '#1', title: 'dasha', weight: '1' } }, { lastID: 1 }],
@@ -11,57 +12,105 @@ const stateMachineDefinition = {
   states: {
     initialState: {
       actions: {
-        onEnter: (): void => {
-          const url = globalThis.location.hash || '#/';
-          historyResolver('initial', url);
+        onEnter: function (): void {
+          // вызывается при загрузке приложения
+          //рисуется главная страница
+          // берутся данные из LS
+          // перерисовываются опции
+          historyResolver('main', globalThis.location.hash || '#/');
+          getDataFromLS();
         },
       },
       transitions: {
         addOption: {
           target: 'initialState',
-          action: (): void => {
+          action: function (): void {
+            // вызывать при нажатии на кнопку add option
+            // добавлять данные в LS
+            // перерисовывать страницу
+            setDataToLS(context);
             console.log('add option');
           },
         },
         removeOption: {
           target: 'initialState',
-          action: (): void => {
+          action: function (): void {
+            // вызывать на кнопку delete
+            // удалять выбранную запись в LS в list, но не трогать lastID
+            // перерисовывать страницу
             console.log('remove option');
           },
         },
-        saveOptionsToLSState: {
+        clearList: {
           target: 'initialState',
-          actions: (): void => {
-            // setDataToLS(this.context.optionList);
+          action: function (): void {
+            //вызывать при нажатии на clear list
+            // обновлять данные в LS (оставлять ключи, очищать объекты list, lastID)
+            // перерисовывать опции
+            console.log('clear list');
           },
         },
-        loadOptionsFromLSState: {
+        pasteList: {
           target: 'initialState',
-          actions: (): void => {
-            //getDataFromLS();
+          action: function (): void {
+            // при нажатии на кнопку paste list
+            // открывать модалку, в ней инпут текст, кнопка отмены, кнопка подтвердить,
+            // валидация ввода ( разделение по последней запятой)
+            // обновление LS
+            // перерисовывать опции
+            console.log('paste list');
+          },
+        },
+        saveToFile: {
+          target: 'initialState',
+          action: function (): void {
+            // при нажатии на кнопку save to file
+            // (перенести сюда создание блоба?)
+            // сохранить данные из LS в JSON
+            console.log('saveToFile');
+          },
+        },
+        loadFromFile: {
+          target: 'initialState',
+          action: function (): void {
+            // вызывать при нажатии load from file
+            // читать данные из JSON
+            // удалять все данные из LS
+            // добавлять новые данные в LS
+            // обновлять форму с опциями - id, title, weight =>
+            // перерисовывать страницу? у ромы изменения происходят на закрытие / обновление страницы
+            console.log('loadFromFile');
           },
         },
         start: {
           target: 'decisionPickerState',
-          action: (): void => {
-            console.log('start');
+          action: function (): void {
+            // вызывать при нажатии на кнопку start
+            // если в LS длина листа менее 2 или вес одной из опций менее 1
+            // показываем модалку с текстом "Please add at least 2 valid options.
+            // An option is considered valid if its title is not empty and its weight is greater than 0"
+            // и кнопкной закрыть
+            // иначе
+            // переход на страницу с колесом
+            historyResolver('decisionPicker', globalThis.location.hash || '#/decision-picker');
           },
         },
       },
     },
     errorState: {
       actions: {
-        onEnter: (): void => {
-          const url = globalThis.location.hash || '#/';
-          historyResolver('', url);
+        onEnter: function (): void {
+          // вызывается при переходе по неверному URL
+          // рисуется страница error
+          historyResolver('main', globalThis.location.hash || '#/');
         },
       },
       transitions: {
         returnToMain: {
           target: 'initialState',
-          action: (): void => {
-            const url = globalThis.location.hash || '#/';
-            historyResolver('initial', url);
+          action: function (): void {
+            // вызывать при нажатии кнопки back to main
+            historyResolver('main', globalThis.location.hash || '#/');
           },
         },
       },
@@ -69,6 +118,9 @@ const stateMachineDefinition = {
     decisionPickerState: {
       actions: {
         onEnter: (): void => {
+          // вызывается при переходе на страницу decision picker
+          // берутся данные о заголовказ и весе берутся из LS
+          // отрисовывается колесо
           const url = globalThis.location.hash || '#/decision-picker';
           historyResolver('Decision picker', url);
         },
@@ -76,20 +128,25 @@ const stateMachineDefinition = {
       transitions: {
         returnToMain: {
           target: 'initialState',
-          action: (): void => {
-            const url = globalThis.location.hash || '#/';
-            historyResolver('initial', url);
+          action: function (): void {
+            // вызывать при нажатии кнопки back to main
+            historyResolver('main', globalThis.location.hash || '#/');
           },
         },
         toggleSounds: {
           target: 'decisionPickerState',
-          action: (): void => {
+          action: function (): void {
+            // вызывать при нажатии кнопки sound:...
+            // обновлять LS
             console.log('toggle sounds');
           },
         },
         pick: {
           target: 'decisionPickerState',
-          action: (): void => {
+          action: function (): void {
+            // вызывать при нажатии кнопки pick
+            // добавить функцию rorateWheel сюда
+            // еще одна машина? с состояниями колеса
             console.log('start');
           },
         },
@@ -109,5 +166,5 @@ console.log(stateMachineDefinition);
 // lastID?: undefined; } | { ...; })[]; sou...'.<br/>Types of property 'initialState' are incompatible.<br/>Type
 // 'string' is not assignable to type '&quot;initialState&quot; | &quot;decisionPickerState&quot; | &quot;errorState&quot;'.
 
-// const machine = new StateMachine(stateMachineDefinition, context);
+// const machine = new StateMachine(stateMachineDefinition, null);
 // export { machine };
